f7dee33ed124af8f1abc0cdc534b11f8f2950307
dodimarthen <dodimartin.sit@gmail.com>
2025-02-14 10:34:58 +0700

    Add: add new component

---
+++ src/components/Card.jsx
@@ -0,0 +1,29 @@
+import PropTypes from "prop-types";
+
+export default function Card({ title, description, link }) {
+  return (
+    <a
+      href={link || "#"}
+      className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-sm hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+    >
+      <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+        {title}
+      </h5>
+      <p className="font-normal text-gray-700 dark:text-gray-400">
+        {description}
+      </p>
+    </a>
+  );
+}
+
+
+Card.propTypes = {
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  link: PropTypes.string,
+};
+
+
+Card.defaultProps = {
+  link: "#",
+};
