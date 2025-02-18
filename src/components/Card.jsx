4d447ac3c827e25ca8c9ffa1ee6f3ec1fdc762d8
dodimarthen <dodimartin.sit@gmail.com>
2025-02-18 14:42:23 +0700

    modify : add transition effect

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,18 +3,19 @@ import PropTypes from "prop-types";
 export default function Card({ title, icon, number, bgColor }) {
   return (
     <a
-      className="flex flex-col max-w-sm p-6 border border-gray-200 rounded-lg shadow-sm hover:opacity-90"
+      className="flex flex-col max-w-sm p-6 border border-gray-200 rounded-lg shadow-sm 
+                 hover:scale-105 transition-transform duration-300 ease-in-out"
       style={{ backgroundColor: bgColor }}
     >
-  
+      {/* Title */}
       <h5 className="text-lg font-semibold text-white mb-4 text-center">{title}</h5>
 
-
+      {/* Icon and Number Side by Side */}
       <div className="flex items-center justify-between">
-    
+        {/* Icon on the Left */}
         <img src={icon} alt={title} className="w-16 h-16" />
 
-
+        {/* Number on the Right */}
         <span className="text-5xl font-bold text-white ml-4">{number}</span>
       </div>
     </a>
